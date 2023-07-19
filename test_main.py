3bf007eb264db41c447113806ebbbb237edcb42a
Ben Wallace <benfwalla@gmail.com>
2023-07-18 22:28:49 -0600

    Add tests

--- test_main.py
+++ test_main.py
@@ -3,6 +3,7 @@ import pytest
 from unittest.mock import patch, MagicMock
 from main import read_existing_jobs, write_new_jobs, get_webpage_html, check_new_replit_jobs
 
+
 def test_read_existing_jobs(tmpdir):
     # Create a temporary CSV file
     p = tmpdir.mkdir("sub").join("jobs.csv")
@@ -10,6 +11,7 @@ def test_read_existing_jobs(tmpdir):
 
     job_ids = read_existing_jobs(p)
     assert job_ids == {'1', '2'}
+
 
 def test_write_new_jobs(tmpdir):
     # Prepare new jobs data
@@ -25,11 +27,13 @@ def test_write_new_jobs(tmpdir):
         lines = list(reader)
     assert lines == [['3', 'Job3', 'Location3', 'URL3'], ['4', 'Job4', 'Location4', 'URL4']]
 
+
 @patch('main.webdriver.Chrome')
 def test_get_webpage_html(mock_chrome):
     mock_chrome().page_source = '<html>Test HTML</html>'
     html = get_webpage_html('https://replit.com/site/careers')
     assert html == '<html>Test HTML</html>'
+
 
 def test_check_new_replit_jobs():
     existing_job_ids = {'1', '2'}
@@ -47,5 +51,6 @@ def test_check_new_replit_jobs():
     """
     new_jobs = check_new_replit_jobs(existing_job_ids, html)
     assert new_jobs == [['3', 'Job3', 'Location3', '/job/3'], ['4', 'Job4', 'Location4', '/job/4']]
+
 
 pytest.main()
